7e5ac14abb8db7c0147a2f7523bfc8baced273e9
Debarshi Ray <debarshir@gnome.org>
2019-06-10 20:30:18 +0200

    Support tracking the active container inside the terminal
    Add sequences
      OSC 777 ; container ; push ; NAME ; RUNTIME BEL
      OSC 777 ; container ; push ; NAME ; RUNTIME ST
      OSC 777 ; container ; pop ; NAME ; RUNTIME BEL
      OSC 777 ; container ; pop ; NAME ; RUNTIME ST
    
    that let container tools notify the terminal emulator when entering and
    leaving a container environment. The RUNTIME argument namespaces the
    NAME and identifies the container tooling being used. eg., docker,
    flatpak, podman, toolbox, etc..
    
    The OSC 777 escape sequence is taken from Enlightenment's Terminology:
    https://phab.enlightenment.org/T1765
    
    It's a VTE-specific extension until a standard escape sequence is
    agreed upon across multiple different terminal emulators [1].
    
    [1] https://gitlab.freedesktop.org/terminal-wg/specifications/issues/17

--- src/vteinternal.hh
+++ src/vteinternal.hh
@@ -58,6 +58,7 @@
 
 #include <list>
 #include <queue>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -134,6 +135,18 @@ typedef enum _VteCharacterReplacement {
         VTE_CHARACTER_REPLACEMENT_LINE_DRAWING,
         VTE_CHARACTER_REPLACEMENT_BRITISH
 } VteCharacterReplacement;
+
+struct VteContainer {
+public:
+        VteContainer(const std::string &name, const std::string &runtime) :
+                m_name{name},
+                m_runtime{runtime}
+        {
+        }
+
+        std::string m_name;
+        std::string m_runtime;
+};
 
 typedef struct _VtePaletteColor {
 	struct {
@@ -577,6 +590,9 @@ public:
         gboolean m_adjustment_value_changed_pending;
         gboolean m_cursor_moved_pending;
         gboolean m_contents_changed_pending;
+
+        bool m_containers_changed{false};
+        std::stack<VteContainer> m_containers;
 
         /* desktop notification */
         bool m_notification_received{false};
